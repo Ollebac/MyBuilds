5da301240577c2d09f9e7a485b7a01d994c8aaed
Ian <iancabello16@gmail.com>
2023-01-23 18:19:45 -0500

    Added remove item function

--- src/SearchList.jsx
+++ src/SearchList.jsx
@@ -4,54 +4,94 @@ import { useState, useRef } from 'react';
 
 export default function SearchList() {
 
-  const [items,  setItems] = useState(["Old School Runescape", "League of Legends", "Halo"])
-  const [query, setQuery] = useState("");
-  const inputRef = useRef();
+  const [items,  setItems] = useState(['Old School Runescape', 'League of Legends', 'Halo'])
+  const [query, setQuery] = useState('');
+  const addRef = useRef();
+  const removeRef = useRef();
 
   const filteredItems = items.filter(item => {
     return item.toLowerCase().includes(query.toLowerCase());
   });
 
-  function onSubmit(e) {
+  function addSubmit(e) {
     e.preventDefault();
 
-    const value = inputRef.current.value
-    if (value === "") return;
+    const value = addRef.current.value
+    if (value === '') return;
     setItems(prev => {
       return [...prev, value];
     })
-    inputRef.current.value = "";
+    addRef.current.value = '';
+  }
+
+  function removeSubmit(e) {
+    e.preventDefault();
+
+    console.log('clicked remove');
+    const removeQuery = removeRef.current.value;
+    let index = -1;
+
+    items.some((value, i) => {
+      if (removeQuery.toLowerCase() === value.toLowerCase()) {
+        index = i;
+        console.log(`Found and removed '${removeQuery}' at index: ${index}`)
+        setItems(prev => prev.filter((val, i) => i !== index));
+      }
+    });
+    
+    if (!index) {
+      console.log(`${removeQuery} is not in the list. Try again.`)
+    }
+    
+    removeRef.current.value = ''
   }
 
   function onChange(e) {
     setItems(prev => {
       const value = e.target.value;
       return prev.filter(item => 
-        item.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+        item.toLowerCase().includes(value.toLowerCase())
       )
     })
   }
 
   return (
-    <>
+    <main className='search_list_page'>
+      <br />
+      <header>
+        <p className='challenge'> 
+          <strong>Challenge: </strong> 
+          <i>Create a dynamic list that can be modified and that can be filtered via a search bar.</i>
+        </p>
+      </header>
       <br />
       <br />
-      Search:  
-      <input 
-        value={query} 
-        onChange={e => setQuery(e.target.value)} 
-        type="search" 
-      />
+      <div className='search'>
+        <strong>Search:</strong> &nbsp; 
+        <input 
+          value={query} 
+          onChange={e => setQuery(e.target.value)} 
+          type='search' 
+        />
+      </div>
       <br />
-      <br />
-      <form onSubmit={onSubmit}>
-        New Item: <input ref={inputRef} type="text" />
-        <button type="submit"> Add </button>
+      <form onSubmit={addSubmit}>
+        Add Item: <input ref={addRef} type='text' />
+        <button type='submit'> Add </button>
       </form>
-      <h3 className="list_title"> Favorite Games </h3>
-      {filteredItems.map(item => (
-        <div>{item}</div>
-      ))}
-    </>
+      <form onSubmit={removeSubmit}>
+        Remove Item: <input ref={removeRef} type='text' />
+        <button type='submit'> Remove </button>
+      </form>
+      <div className='solution'>
+        <h4 className='title'> Favorite Games </h4>
+        {filteredItems.map((item, index) => (
+          <div className='list_item'>
+            <strong> {index + 1}: </strong>
+            {item}
+          </div>
+        ))}
+      </div>
+    </main>
   )
 };
