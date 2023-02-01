f5b727a69f7bdf14a6df867f173e01b5a76c1fee
Ian <iancabello16@gmail.com>
2023-02-01 15:12:56 -0500

    Cleanup

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Homepage from './Homepage';
 import Potionomics from './Potionomics';
 import SearchList from "./SearchList";
-import FetchLeague from "./FetchLeague";
-// import PuppetScraping from "./PuppetScraping";
 import MyGear from "./MyGear";
 
 import './App.css';
@@ -12,16 +10,14 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div className="home_container">
+      <span className="home_container">
         <a className="home_link" href="/">To Home</a>
-      </div>
+      </span>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/potionomics" element={<Potionomics />} />
         <Route path="/searchlist" element={<SearchList />} />
-        <Route path="/leagueoflegends" element={<FetchLeague />} />
         <Route path="/mygear" element={<MyGear />} />
-        {/* <Route path="/puppetscraping" element={<PuppetScraping />} /> */}
       </Routes>
     </Router>
   );
